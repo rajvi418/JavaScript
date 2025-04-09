cd9c909af1f03fd0221deb2cfa207e9da818d5ec
Rajvi Undhad <undhadrajviii@gmail.com>
2025-04-09 11:25:55 +0530

    Add initial files for factorial calculation and login form

---
+++ Login/JS/login.js
@@ -0,0 +1,9 @@
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
+
+if (username === 123 && password === 456) {
+    alert("Login Successful");
+}
+else {
+    alert("Login Failed");
+}
