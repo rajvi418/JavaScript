1ec76264eae2222d1c672f004d85c676fc9f440a
Rajvi Undhad <undhadrajviii@gmail.com>
2025-04-10 09:21:17 +0530

    Refactor login validation logic for clarity and accuracy

--- Login/JS/login.js
+++ Login/JS/login.js
@@ -8,20 +8,18 @@ let password = 456;
 //     console.log("Login Failed");
 // }
 
-if (username && password) {
+if (username === 123 && password === 456) {
     if (username === 123) {
-
         console.log("Correct UserName!");
 
         if (password === 456) {
-
             console.log("Correct Password!");
 
             if (username === 123 && password === 456) {
                 console.log("Login Successfull !");
             }
             else {
-                console.log("Incorrect Username and Password !");
+                console.log("");
             }
         }
         else {
@@ -33,5 +31,5 @@ if (username && password) {
     }
 }
 else {
-    console.log("Login Fail");
+    console.log("Incorrect Username and Password !");
 }
