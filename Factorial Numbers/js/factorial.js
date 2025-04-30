bcfb29d072b95d379664329cc0e315376fee0e12
Rajvi Undhad <undhadrajviii@gmail.com>
2025-04-30 14:01:59 +0530

    Add initial HTML structure and JavaScript for factorial calculation

---
+++ Factorial Numbers/js/factorial.js
@@ -0,0 +1,7 @@
+// Factorial of a number using for loop
+let i = 10;
+let factorial = 1;
+for (let j = 1; j <= i; j++) {
+    factorial = factorial * j;
+    console.log("Factorial of " + j + " is " + factorial);
+}
