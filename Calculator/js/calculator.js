9315e942f2424cdd9c04119f27a3f71e0be762e1
Rajvi Undhad <undhadrajviii@gmail.com>
2025-05-07 11:17:36 +0530

    Add CSS styling and HTML structure for calculator interface

---
+++ Calculator/js/calculator.js
@@ -0,0 +1,24 @@
+let value1;
+let value2;
+let answer;
+
+function add() {
+    value1 = document.getElementById("value1").value1;
+    value2 = document.getElementById("value2").value2;
+    answer = value1 + value2;
+}
+function sub() {
+    value1 = document.getElementById("value1").value1;
+    value2 = document.getElementById("value2").value2;
+    answer = value1 - value2;
+}
+function multiply() {
+    value1 = document.getElementById("value1").value1;
+    value2 = document.getElementById("value2").value2;
+    answer = value1 * value2;
+}
+function divide() {
+    value1 = document.getElementById("value1").value1;
+    value2 = document.getElementById("value2").value2;
+    answer = value1 / value2;
+}
